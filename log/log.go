5cdc2b87ec9d9a16e226d7497a0a19de171da104
465583030 <465583030@qq.com>
2017-08-27 07:14:51 +0800

    fix log issue

--- log/log.go
+++ log/log.go
@@ -3,8 +3,8 @@ package log
 import (
 	"fmt"
 
-	"github.com/beewit/beekit/conf"
 	"github.com/astaxie/beego/logs"
+	"github.com/beewit/beekit/conf"
 )
 
 var Logger = logs.GetBeeLogger()
@@ -16,13 +16,13 @@ var (
 func init() {
 	conf := fmt.Sprintf(
 		`{
-		  "filename": "%s",
-	      "maxdays": %s,
-	      "daily": %s,
-		  "rotate": %s,
-	      "level": %s,
-	      "separate": %s
-	 	}`,
+			"filename": "%s",
+			"maxdays": %s,
+			"daily": %s,
+			"rotate": %s,
+			"level": %s,
+			"separate": "[%s]"
+		}`,
 		cfg.Get("logger.path"),
 		cfg.Get("logger.maxdays"),
 		"true",
@@ -30,6 +30,7 @@ func init() {
 		cfg.Get("logger.level"),
 		cfg.Get("logger.separate"),
 	)
+	fmt.Println(conf)
 
 	logs.SetLogger(logs.AdapterMultiFile, conf)
 	logs.SetLogger("console")
