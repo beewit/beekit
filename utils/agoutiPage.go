f3f4be4e65a63a85a651dda898414687ce0c8f07
zxbit2011 <zxbit2011@163.com>
2017-09-27 10:48:31 +0800

    --

---
+++ utils/agoutiPage.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/sclevine/agouti"
+	"github.com/sclevine/agouti/api"
+)
+
+type AgoutiPage struct {
+	*agouti.Page
+}
+// SwitchToRootFrame focuses on the original, default page frame before any calls
+// to Selection.Frame were made. After switching, all new and existing selections
+// will refer to the root frame. All further Page methods will apply to this frame
+// as well.
+func (p *AgoutiPage) SwitchToRootFrameByName(frame *api.Element) error {
+	if err := p.Session().Frame(frame); err != nil {
+		return fmt.Errorf("failed to switch to original page frame: %s", err)
+	}
+	return nil
+}
