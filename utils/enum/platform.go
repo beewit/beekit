0c9904ca329f8adcd551f47b486f88eb8ad93235
zxbit2011 <zxbit2011@163.com>
2017-11-11 09:48:23 +0800

    --

--- utils/enum/platform.go
+++ utils/enum/platform.go
@@ -1,9 +1,14 @@
 package enum
 
 const (
-	WECHAT = "微信"
-	ALIPAY = "支付宝"
-	WEIBO  = "新浪微博"
-	TAOBAO = "淘宝"
-	QQ     = "QQ"
+	WECHAT    = "微信"
+	ALIPAY    = "支付宝"
+	WEIBO     = "新浪微博"
+	TAOBAO    = "淘宝"
+	QQ        = "QQ"
+	WECHAT_ID = 17
+	//ALIPAY_ID = "支付宝"
+	WEIBO_ID  = 10
+	TAOBAO_ID = 13
+	QQ_ID     = 18
 )
